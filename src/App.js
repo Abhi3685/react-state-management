23a380f25f41d27c13256cdf71560178cc1bb412
Abhishek Bindra <abhishek.bindra.ab@gmail.com>
2020-04-06 16:32:10 +0530

    State mgmt using React Redux

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import React from 'react';
-
-import { UserProvider, UserConsumer } from './UserContext';
+import { UserProvider } from './context/UserContext';
+import ContextApi from './components/ContextApi';
+import ReduxState from './components/ReduxState';
 
 function App() {
   return (
-    <UserProvider>
-      <div style={{ textAlign: "center" }}>
+    <div style={{ textAlign: "center" }}>
+      {/* Context Api Demo */}
+
+      <UserProvider>
         <h1>React State Management using Context Api</h1>
-        <UserConsumer>
-          {( { user, setUser } ) => (
-            user.email == '' ? <div>
-              {/* Render Login Screen */}
-              <button onClick={() => setUser({ name: 'Abhishek', email: 'abhi@gmail.com', phone: '9876543210' }) }>Log me in</button>
-            </div> : <div>
-              {/* Render User Screen */}
-              <button onClick={() => setUser({ name: '', email: '', phone: '' }) }>Log me out</button>
-              <p>Name: { user.name }</p>
-              <p>Email: { user.email }</p>
-              <p>Phone: { user.phone }</p>
-            </div>
-          )}
-        </UserConsumer>
-      </div>
-    </UserProvider>
+        <ContextApi />
+      </UserProvider>
+
+      <br/><hr/>
+
+      {/* React Redux Demo */}
+
+      <h1>React State Management using Redux</h1>
+      <ReduxState />
+    </div>
   );
 }
 
